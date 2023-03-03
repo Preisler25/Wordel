e6f325bdbab54a2f849c1aa070a2df40ec6820e7
Preisler <preisler.andras@gmail.com>
2023-03-03 23:51:47 +0100

    working

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,18 +1,48 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class MainFrame extends JFrame{
     private JPanel JPmain;
     private JButton btnOk;
+    private JTextField JtextW1;
+    private JTextField JtextW2;
+    private JTextField JtextW5;
+    private JTextField JtextW3;
+    private JTextField JtextW4;
+
+    private ArrayList<JTextField> JtextW = new ArrayList<JTextField>();
+
 
     public MainFrame(){
+        JtextW.add(JtextW1);
+        JtextW.add(JtextW2);
+        JtextW.add(JtextW3);
+        JtextW.add(JtextW4);
+        JtextW.add(JtextW5);
+
         setContentPane(JPmain);
-        setTitle("Szia");
+        setTitle("Wordel");
         setSize(400, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String word = getWord();
+                System.out.println("Hello " + word);
+            }
+        });
     }
-    public static void main(String[] args){
-        MainFrame mf
+    public String getWord(){
+        String temp = "";
+        for (JTextField jTextW : JtextW) {
+            temp += jTextW.getText();
+            jTextW.setText("");
+        }
+        return temp;
     }
 }
 
