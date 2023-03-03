6c00f9a95acb5a7d586620b264ff9711b9d6c109
Preisler <preisler.andras@gmail.com>
2023-03-03 20:00:21 +0100

    main

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,2 +1,21 @@
-public class MainFrame {
+import javax.swing.*;
+
+public class MainFrame extends JFrame{
+    private JPanel JPmain;
+    private JButton btnOk;
+
+    public MainFrame(){
+        setContentPane(JPmain);
+        setTitle("Szia");
+        setSize(400, 400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        MainFrame mf
+    }
 }
+
+
+
+
